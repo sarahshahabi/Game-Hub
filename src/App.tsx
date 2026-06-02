8c340c9900be9ecf5a78b1ae3b2cefdaf18861da
Sarah <ssarashahabii@gmail.com>
2026-06-02 11:58:34 +0330

    Implement Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {  Grid, GridItem, Show } from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
+import { ColorModeButton } from "./components/ui/color-mode"
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       lg:`"nav nav nav" "aside main main"`
     }}>
       <GridItem area="nav">
-        <NavBar/>
+        <NavBar />
+        <ColorModeButton/>
       </GridItem>
    
         <GridItem area="aside" bg="gold" display={{base:"none", lg:"block"}}>
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { ColorModeProvider } from './components/ui/color-mode.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider>
         <App />
+      </ColorModeProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
