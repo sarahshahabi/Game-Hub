cb08badb5a2b65443e0b5bdb9c883b473f9c3a1a
Sarah <ssarashahabii@gmail.com>
2026-06-02 18:26:19 +0330

    Build a color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import {  Grid, GridItem, Show } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
-import { ColorModeButton } from "./components/ui/color-mode"
+import ColorModeSwitch from "./components/ColorModeSwitch"
 
 function App() {
   return (
     <Grid templateAreas={{
       base: `"nav" "main"`,
-      lg:`"nav nav nav" "aside main main"`
+      lg: `"nav nav nav" "aside main main"`
     }}>
       <GridItem area="nav">
         <NavBar />
-        <ColorModeButton/>
       </GridItem>
-   
-        <GridItem area="aside" bg="gold" display={{base:"none", lg:"block"}}>
-          Aside
-        </GridItem>
 
- 
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="aside"  display={{ base: "none", lg: "block" }}>
+        Aside
+      </GridItem>
+
+
+      <GridItem area="main">
         Main
       </GridItem>
-  </Grid>
+    </Grid>
   )
 }
 
---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,17 @@
+import { HStack, Switch } from '@chakra-ui/react'
+import { useColorMode } from './ui/color-mode'
+
+function ColorModeSwitch() {
+const {toggleColorMode, colorMode} = useColorMode()
+  return (
+    <HStack>
+      <Switch.Root colorPalette="green" defaultChecked={true} checked={colorMode === "dark"} onChange={toggleColorMode }>
+        <Switch.HiddenInput />
+        <Switch.Control />
+        <Switch.Label>Dark Mode</Switch.Label>
+      </Switch.Root>
+    </HStack>
+  )
+}
+
+export default ColorModeSwitch
