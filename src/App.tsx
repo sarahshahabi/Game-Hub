6d297a4286826268cd4bb1a4b7eee7ae571cb0d2
Sarah <ssarashahabii@gmail.com>
2026-07-19 12:04:32 +0330

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import GenreList from "./components/GenreList"
 import { useState } from "react"
 import { Genre } from "./hooks/useGenres"
 import PlatformSelector from "./components/PlatformSelector"
+import { Platform } from "./hooks/useGames"
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
 
   return (
     <Grid templateAreas={{
@@ -24,13 +26,13 @@ function App() {
       </GridItem>
 
       <GridItem area="aside" display={{ base: "none", lg: "block" }} paddingX={5}>
-        <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} selectedGenre={selectedGenre}/>
+        <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} selectedGenre={selectedGenre} />
       </GridItem>
 
 
       <GridItem area="main">
-        <PlatformSelector/>
-        <GameGrid selectedGenre={selectedGenre}/>
+        <PlatformSelector onSelectPlatform={(platform) => setSelectedPlatform(platform)} selectedPlatform={selectedPlatform} />
+        <GameGrid selectedGenre={selectedGenre} selectedPlatform={selectedPlatform} />
       </GridItem>
     </Grid>
   )
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,15 +1,23 @@
 import { Button, Menu, Portal } from '@chakra-ui/react'
 import { BsChevronRight } from 'react-icons/bs'
 import usePlatforms from '../hooks/usePlatforms'
+import { Platform } from '../hooks/useGames'
 
-function PlatformSelector() {
+
+interface Props {
+  onSelectPlatform: (platform: Platform) => void
+  selectedPlatform: Platform | null
+}
+
+
+function PlatformSelector({onSelectPlatform, selectedPlatform} : Props) {
   const { data, error, isLoading } = usePlatforms()
   if (error) return null
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button size="sm" variant="outline">
-          Platforms
+          { selectedPlatform?.name || "Platforms"}
           <BsChevronRight />
         </Button>
       </Menu.Trigger>
@@ -17,7 +25,7 @@ function PlatformSelector() {
         <Menu.Positioner>
           <Menu.Content>
             {
-              data.map(platform => <Menu.Item value={platform.name} key={platform.id}>{platform.name}</Menu.Item>)
+              data.map(platform => <Menu.Item onClick={() => onSelectPlatform(platform)} value={platform.name} key={platform.id}>{platform.name}</Menu.Item>)
             }
           </Menu.Content>
         </Menu.Positioner>
